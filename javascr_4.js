7132ead41e74b17d3ae1c0357c279592fb5e5853
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-13 21:04:48 +0500

    declaring variables

---
+++ javascr_4.js
@@ -0,0 +1,16 @@
+
+/**declaring variables */
+let hertz="zindabad"
+console.log(hertz)
+
+/**another method is var */
+var nationalty="pakistan";
+var age=15;
+var nationalty=60.05;
+var nationalty=true;
+
+/**to see var as output u can use console or alert 
+alert(nationalty);
+console.log(age);  */
+alert(nationalty);
+console.log(age)
