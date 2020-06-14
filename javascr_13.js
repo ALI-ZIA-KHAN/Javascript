91801ffb401bd9d66265d24c8afbd370fae70881
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-14 20:07:52 +0500

    Equality checkers

---
+++ javascr_13.js
@@ -0,0 +1,17 @@
+/**equality check */
+/**==: compare w/o considering datatype */
+/**===:first check datatype before comparing */
+var a=3;
+console.log(a==6);
+console.log(a===6);
+console.log(a!=6);
+console.log(a!==6);
+
+
+
+
+
+c=5
+d="5"
+console.log(c==d)
+console.log(c===d)
