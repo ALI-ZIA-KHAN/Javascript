3e52963baecbd53b7bfe6689b5a4ca403cdb86e0
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-14 18:43:34 +0500

    FOR LOOP IN JS

---
+++ javascr_10.js
@@ -0,0 +1,16 @@
+/**for loop */
+/**for(initialization;condition;expression) {
+ * //code to be executed
+ * }*/
+for (var i=0;i<3;i++){
+    console.log(i);
+
+}
+
+
+console.log("now another example")
+
+for (var i=10;i>0;i--){
+    console.log(i)
+}
+/**these all 3 statements are optional but then loop will be infinite */
