3a67d5ff534624ea8c27fd20073634b6e667ccec
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-14 20:40:18 +0500

    LOGICAL OPERATORS OR AND NOT

---
+++ javascr_14.js
@@ -0,0 +1,16 @@
+/**logical operators */
+/**AND */
+var a=60;
+var b=a>50 && a<70;1
+console.log(b)
+
+/**Or */
+var c=60;
+var d=c>50 || c<70;
+console.log(d)
+
+
+/**logical NOT */
+m=60;
+n=!(m<50);
+console.log(n);
