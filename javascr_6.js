c16b4d030d16b35398151c11d1d5bc32dead8700
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-14 14:00:29 +0500

    INCR/DECR OPERATORS but look all related files bcz complex concept

---
+++ javascr_6.js
@@ -0,0 +1,25 @@
+/**Increment n decrement operator */
+/**it is a complex concept so look all files related to this in order to clear your concept */
+var a=6
+a++
+console.log("a++",a)
+
+var b=6
+++b
+console.log("++b",b)
+
+
+var c=6
+--c
+console.log("--c",c)
+
+
+var c=6
+c--
+console.log("c--",c)
+
+
+var e=6
+var f=3
+c=++e - --f + --e
+console.log(c)
