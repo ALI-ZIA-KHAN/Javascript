69b65f89e739e6467ce0d56de40c0d3f7ca75571
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-14 13:47:59 +0500

    Data types in JS

---
+++ javascr_5.js
@@ -0,0 +1,27 @@
+/**DATA TYPES */
+/**SIX DATA TYPES ARE PRIMITIVE */
+/**  1)FOR STRING */
+var name="Mark";
+/**while updation no var */
+name="John";
+console.log(name);
+/**see carefully */
+var mess ='it"s alright';
+/**OR */
+var mess="it's alright";
+/**2)FOR BOOLEAN */
+var feepaid=true;
+/** 3) FOR NUMBERS */
+var a=6;
+var b=12;
+var c=a+b;
+/** 4) undefined*/
+var nail;
+/**OR */
+var nail="Mark"
+nail=undefined;
+/**5) NULL */
+var lazy=null;
+/**Difference is there b/w null n undefined */
+/** 6) symbol*/
+/**A unique value that is not equal to other value */
