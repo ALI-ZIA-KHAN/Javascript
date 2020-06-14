6de0abd772dbf44f4645996bc1413043c94d3e03
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-14 14:21:52 +0500

    Concatenating basics n prompt for user input

---
+++ javascr_7.js
@@ -0,0 +1,27 @@
+/**Concatenating strings*/
+var fname="Ali"
+var mname="Zia"
+var lname="Khan"
+var fullname=fname +" "+mname +" "+lname
+alert(fullname)
+
+
+var a="6" +2
+console.log(a);
+var b="hello" +2
+console.log(b);
+var c=5+9
+console.log(c);
+var d= 3+4+"pakistan"
+console.log(d);
+var e="pakistan"+3+4
+console.log(e);
+var f="pakistan" + (3+4)
+console.log(f);
+
+
+/**Prompt to take input */
+var age=prompt("What is your age")
+console.log(age)
+var newage=age+5
+console.log(newage)
