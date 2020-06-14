2fce046c9860e94d565f126e72190b5d0386c91e
ALI ZIA KHAN <azk443.443@gmail.com>
2020-06-14 18:34:18 +0500

    Very imp file to see postfix/prefix operator

---
+++ javascr_9.js
@@ -0,0 +1,12 @@
+/**prefix operator : first incr/decr & then assign */
+var age=12;
+var newage=++age;
+console.log(age);
+console.log(newage);
+
+
+/**postfix first assign then inc/decr */
+var first=14;
+var last=first++;
+console.log(first);
+console.log(last);
